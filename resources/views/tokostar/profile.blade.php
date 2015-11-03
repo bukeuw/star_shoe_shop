45473cf372817505cf034412d917439b89c22c42
Dina Cheng <cheng.dina@yahoo.com>
2015-11-03 07:24:12 +0700

    Add view for display or user profile

---
+++ resources/views/tokostar/profile.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.userpage')
+
+@section('title', 'Home')
+
+@section('page-content')
+	<div class="row">
+
+		<div class="col-md-9">
+			<div class="panel panel-dark">
+				//
+			</div>
+		</div>
+		
+		@include('layouts.partials.sidebar')
+
+	</div>
+@endsection
