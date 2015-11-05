cd1d308da4b859062ad4497eda1e26bf05ff1788
Dina Cheng <cheng.dina@yahoo.com>
2015-11-05 15:17:50 +0700

    Duplicate item

--- resources/views/emails/reset.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.mail')
-
-@section('mail-content')
-	<h1>Link Reset</h1>
-	<p>Silahkan klik link ini untuk mereset password Anda: {{ url('password/reset/'.$token) }}</p>
-@endsection
