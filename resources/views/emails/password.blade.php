5cd86d3f2d70b5e36e26b95eb0fd6b00f155950b
Dina Cheng <cheng.dina@yahoo.com>
2015-11-05 15:53:29 +0700

    Change email template layout

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.mail')
-
-@section('mail-content')
+<div class="message-content">
 	<h1>Link Reset</h1>
 	<p>Silahkan klik link ini untuk mereset password Anda: <a href="{{ url('password/reset/'.$token) }}">{{ url('password/reset/'.$token) }}</a></p>
-@endsection
+</div>
