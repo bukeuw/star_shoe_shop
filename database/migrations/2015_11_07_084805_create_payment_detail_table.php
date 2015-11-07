9667b609cc18d3461ae93b3b6df0ae3136da85f3
Dina Cheng <cheng.dina@yahoo.com>
2015-11-07 19:06:44 +0700

    Create migration for payment detail table

---
+++ database/migrations/2015_11_07_084805_create_payment_detail_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_detail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('bank');
+            $table->string('account_number');
+            $table->string('account_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('payment_detail');
+    }
+}
