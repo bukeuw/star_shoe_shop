e03cc0562e60742c94059a6c8d482ba5fb7d9736
Dina Cheng <cheng.dina@yahoo.com>
2015-11-07 19:03:56 +0700

    Create migration for transaction table

---
+++ database/migrations/2015_11_07_070025_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->integer('user_id');
+	    $table->string('payment_method');
+	    $table->decimal('total', 8, 0);
+	    $table->boolean('comfirmed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
