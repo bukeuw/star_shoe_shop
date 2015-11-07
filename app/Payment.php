52ffe970a45081d6c13259614a7a9180b4db2ab1
Dina Cheng <cheng.dina@yahoo.com>
2015-11-08 04:25:17 +0700

    Add Transaction model

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    $table = 'payment_detail';
+    $fillable = ['bank', 'account_number', 'account_name', 'transaction_id'];
 }
