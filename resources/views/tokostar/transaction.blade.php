ccad522ad6c1d27d44cab253eda01ba4106c119e
Dina Cheng <cheng.dina@yahoo.com>
2015-11-08 12:51:25 +0700

    Add transaction view for member

---
+++ resources/views/tokostar/transaction.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.adminpage')
+
+@section('title', 'Daftar Transaksi')
+
+@section('page-content')
+
+	@include('layouts.partials.messagebag')
+
+	<h1 class="content-header">Produk</h1>
+	@if (count(Auth::user()->transactions) > 0)
+		<div class="table-responsive">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Tanggal</th>
+						<th>Total Bayar</th>
+						<th>Metode Pembayaran</th>
+						<th>Status</th>
+						<th>Detail</th>
+					</tr>
+				</thead>
+				<tbody>
+				@foreach (Auth::user()->transactions as $transaction)
+					<tr>
+						<td>{{ $transaction->create_at->toDateString() }}</td>
+						<td>{{ $transaction->total_pay }}</td>
+						<td>{{ $transaction->payment_method }}</td>
+						<td>{{ $transaction->confirmed? 'Sukses':'Pending' }}</td>
+						<td>
+							<a href="/member/{{ $transaction->id }}" class="btn btn-default">Lihat Detail</a>
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+		</div>
+	@else
+		<p>Tidak ada transaksi</p>
+		<a href="/product" class="btn btn-default">Lanjut Belanja</a>
+	@endif
+@endsection
+
+@section('custom-js')
+	@include('layouts.partials.messagebagscript')
+@endsection
---
+++ resources/views/tokostar/transactiondetail.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.adminpage')
+
+@section('title', 'Daftar Transaksi')
+
+@section('page-content')
+
+	@include('layouts.partials.messagebag')
+
+	<h1 class="content-header">Produk</h1>
+	@if (count($transaction->details) > 0)
+		<p>Nama: {{ Auth::user()->name }}</p>
+		<p>Tgl Transaksi: {{ $transaction->create_at->toDateString() }}</p>
+		<p>Metode Pembayaran: {{ $transaction->payment_method }}</p>
+		<div class="table-responsive">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Nama Barang</th>
+						<th>Jumlah</th>
+						<th>Harga Satuan</th>
+						<th>Subtotal</th>
+					</tr>
+				</thead>
+				<tbody>
+				@foreach ($transaction->details as $detail)
+					<tr>
+						<td>{{ $detail->product->name }}</td>
+						<td>{{ $detail->quantity }} {{ $detail->product->unit }}</td>
+						<td>{{ $detail->product->price }}</td>
+						<td>Rp. {{ ($detail->product->price * $detail->quantity) }}</td>
+					</tr>
+				@endforeach
+				</tbody>
+				<tfoot>
+					<th>Total Bayar</th>
+					<th colspan="3">
+						<p class="text-right">Rp. {{ $transaction->total_pay }}</p>
+					</th>
+				</tfoot>
+			</table>
+		</div>
+	@endif
+@endsection
+
+@section('custom-js')
+	@include('layouts.partials.messagebagscript')
+@endsection
