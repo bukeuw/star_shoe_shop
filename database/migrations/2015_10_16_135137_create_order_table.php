81e72a5abda0e17821129f31b99e2e0314ec55eb
Dina Cheng <cheng.dina@yahoo.com>
2015-11-12 06:39:35 +0700

    total paid computed in controller not saved into database

--- database/migrations/2015_10_16_135137_create_order_table.php
+++ database/migrations/2015_10_16_135137_create_order_table.php
@@ -15,7 +15,6 @@ class CreateOrderTable extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->decimal('total_paid', 8, 0);
             $table->timestamps();
         });
     }
