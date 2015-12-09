001820344beb7941589cef3a1d1847f1cae4f6b7
bukeuw <sntblasius@gmail.com>
2015-12-09 13:30:43 +0700

    add view for laravel-dompdf

---
+++ resources/views/tokostar/transactionreceipt.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="container-fluid">
+		<h1 class="content-header">Transaction Detail</h1>
+		@if (count($transaction->details) > 0)
+			<p>Nama: {{ Auth::user()->name }}</p>
+			<p>Tgl Transaksi: {{ $transaction->created_at->format('j F Y') }}</p>
+			<p>Metode Pembayaran: {{ $transaction->payment_method }}</p>
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Nama Barang</th>
+							<th>Jumlah</th>
+							<th>Harga Satuan</th>
+							<th>Subtotal</th>
+						</tr>
+					</thead>
+					<tbody>
+					@foreach ($transaction->details as $detail)
+						<tr>
+							<td>{{ $detail->product->name }}</td>
+							<td>{{ $detail->quantity }} {{ $detail->product->unit }}</td>
+							<td>{{ $detail->product->price }}</td>
+							<td>Rp. {{ ($detail->product->price * $detail->quantity) }}</td>
+						</tr>
+					@endforeach
+					</tbody>
+					<tfoot>
+						<th>Total Bayar</th>
+						<th colspan="3">
+							<p class="text-right">Rp. {{ $transaction->total }}</p>
+						</th>
+					</tfoot>
+				</table>
+			</div>
+		@endif
+	</div>
+@endsection
