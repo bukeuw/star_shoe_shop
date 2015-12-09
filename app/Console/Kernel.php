df4ce6047fe328e5060e73ffd377e145b86bbb7e
bukeuw <sntblasius@gmail.com>
2015-12-09 13:26:26 +0700

    add job to remove cart item older than 3 days

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\CartItem;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->call(function() {
+            $cartItems = CartItem::all();
+            
+            foreach ($cartItems as $item) {
+                $created = $item->created_at->addDays(3);
+                $expired = Carbon::now();
+
+                if($created->eq($expired)) {
+                    $item->delete();
+                }
+            }
+        })->daily();
     }
 }
